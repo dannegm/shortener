ec6d156178eceaeb0a6c22c0d978099064a8077d
dannegm <dannegm@gmail.com>
2019-11-02 20:00:46 -0600

    Implements Babel for ES2019 compatibility

--- app.js
+++ app.js
@@ -1,80 +1,97 @@
-'use strict';
-require ('colors');
+import 'dotenv/config'
+import 'colors'
 
-const settings = require (`./config/settings`);
-const
-  express = require ('express'),
-  mongoose = require ('mongoose'),
-  winston = require ('winston'),
-  session = require('express-session'),
-  bodyParser = require ('body-parser'),
-  favicon = require('serve-favicon');
+import express from 'express';
+import mongoose from 'mongoose'
+import winston from 'winston'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import favicon from 'serve-favicon'
+import helmet from 'helmet'
+import cors from 'cors'
+import { highlight }  from 'cli-highlight'
 
-//! Setup & expose
-const app = module.exports = express ();
-const MongoStore = require ('connect-mongo') (session);
+import settings from './config/settings'
+import modules from './modules'
 
-app.locals.hostname = settings.server.hostname;
-mongoose.connect (settings.mongo.schema);
+const app = express ()
+
+app.locals.hostname = settings.server.hostname
+mongoose.connect (settings.mongo.schema, settings.mongo.options)
 
 app
-  .use (favicon ('./favicon.ico'))
-  .use (bodyParser.json ())
-  .use (bodyParser.urlencoded ({ extended: false }))
-  .use (session ({
-    store: new MongoStore ({
-      mongooseConnection: mongoose.connection,
-    }),
-    secret: settings.server.secret,
-    resave: true,
-    saveUninitialized: true
-  }));
+    .use (favicon ('./favicon.ico'))
+    .use (helmet ())
+    .use (cors ())
+    .use (bodyParser.json ())
+    .use (bodyParser.urlencoded ({ extended: false }))
+    .use (
+        session ({
+            secret: settings.server.secret,
+            resave: true,
+            saveUninitialized: true,
+        })
+    )
 
-//! Logger
-app.logger = new winston.Logger ({
 
-});
+const { log } = console
+app.logger = new winston.Logger ()
 app.logger.configure ({
-  transports: [
-    new (winston.transports.Console) ({
-      name: 'Console',
-      timestamp: (new Date ()).toISOString (),
-      colorize: true,
-    }),
-    new (winston.transports.File) ({
-      name: 'File',
-      filename: `${process.env.npm_package_name}.log`,
-      timestamp: (new Date ()).toISOString (),
-      json: false,
-      formatter: (options) => {
-        const message = options.message.replace (/(\u001B)|(\\u[0-9a-f]{0,4})|(\[[0-9a-z]{1,3})/gi, '');
-        return `${options.timestamp} - [${options.level.toUpperCase ()}] ${message}`;
-      },
-    }),
-  ],
-});
+    transports: [
+        new (winston.transports.Console) ({
+            name: 'Console',
+            timestamp: (new Date ()).toISOString (),
+            colorize: true,
+        }),
 
-//! Routes
-app.all ('*', (req, res, next) => {
-  app.logger.info (`${req.method.blue.bold} ${req.path}`);
-  next ();
-});
-app.all ('/', (req, res) => {
-  res.json ({
-    method: req.method,
-    message: 'Few steps to greatness.',
-    body: req.body,
-  });
-});
+        new (winston.transports.File) ({
+            name: 'File',
+            filename: `${process.env.npm_package_name}.log`,
+            timestamp: (new Date ()).toISOString (),
+            json: false,
+            formatter: (options) => {
+                const message = options.message.replace (/(\u001B)|(\\u[0-9a-f]{0,4})|(\[[0-9a-z]{1,3})/gi, '');
+                return `${options.timestamp} - [${options.level.toUpperCase ()}] ${message}`;
+            },
+        }),
+    ],
+})
 
-//! Server
-const deploy = () => {
-  const schema = `http://${settings.server.hostname}:${settings.server.port}`;
-  app.listen (settings.server.port);
-  app.logger.info (`Listening on ${schema.yellow}`);
-};
-
-//! Expose port
-if (!module.parent) {
-  deploy ();
+app.outputs = {
+    json (tag, code) {
+        log (`[${tag}]`.magenta.bold)
+        log (highlight (JSON.stringify (code, null, 2)))
+    }
 }
+
+//* Log all request
+app.use ((req, res, next) => {
+    const methods = {
+        'GET': req.method.blue.bold,
+        'POST': req.method.green.bold,
+        'PUT': req.method.yellow.bold,
+        'DELETE': req.method.red.bold,
+        'PATCH': req.method.cyan.bold,
+    }
+    app.logger.info (`${' HTTP '.black.bold.bgWhite} ${methods [req.method]} ${req.path}`)
+    // app.outputs.json ('HEADERS', req.headers);
+    // app.outputs.json ('BODY', req.body);
+    next ()
+});
+
+app.all ('/', (req, res) => {
+    res.json ({
+        method: req.method,
+        message: 'Few steps to greatness.',
+        body: req.body,
+    })
+})
+
+//* Server
+const deploy = () => {
+    const schema = `http://${settings.server.hostname}:${settings.server.port}`
+    app.listen (settings.server.port)
+    app.logger.info (`Listening on ${schema.yellow}`)
+}
+
+deploy ()
--- config/settings.js
+++ config/settings.js
@@ -8,11 +8,20 @@ let server = {
 }
 
 let mongo = {
-  host: process.env.MONGO_HOST,
-  port: process.env.MONGO_PORT,
-  database: process.env.MONGO_DATABASE,
-  user: process.env.MONGO_USER || '',
-  password: process.env.MONGO_PASSWORD || '',
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT,
+    database: process.env.MONGO_DATABASE,
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_PASSWORD || '',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        reconnectTries: Number.MAX_VALUE,   // Never stop trying to reconnect
+        reconnectInterval: 500,             // Reconnect every 500ms
+        poolSize: 10,                       // Maintain up to 10 socket connections
+        bufferMaxEntries: 0,                // If not connected, return errors immediately rather than waiting for reconnect
+        keepAlive: 120,                     // Check TCP socket status every x milliseconds.
+    },
 };
 mongo.auth = mongo.user != '' && mongo.password != '' ? `${mongo.user}:${mongo.password}@` : '';
 mongo.schema = `mongodb://${mongo.auth}${mongo.host}:${mongo.port}/${mongo.database}`;
